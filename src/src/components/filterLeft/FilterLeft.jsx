7cba2695d880d739480706b1f0ffd3122b2b00f5
carlosn97 <carlosnasayov@gmail.com>
2023-04-01 00:05:11 -0500

    GAP-36 landrace crops amlways on screen fixed

--- src/src/components/filterLeft/FilterLeft.jsx
+++ src/src/components/filterLeft/FilterLeft.jsx
@@ -177,6 +177,7 @@ console.log(idss)
           crop={majorCrops}
         ></CheckFilter>
       )}
+      
       {carouselMajorItemsNow && carouselMajorItemsNow.length == 1 && (
         <CheckFilter
           title="Landrace Crops"
@@ -186,6 +187,14 @@ console.log(idss)
         ></CheckFilter>
       )}
       {carouselMajorItemsNow && carouselMajorItemsNow.length > 1 && (
+        <CheckFilter
+          title="Landrace Crops"
+          onDataChange={handleDataLandraceCropChange}
+          onChange={shouldReset}
+          crop={[]}
+        ></CheckFilter>
+      )}
+      {carouselMajorItemsNow && carouselMajorItemsNow.length ==0 && (
         <CheckFilter
           title="Landrace Crops"
           onDataChange={handleDataLandraceCropChange}
