1d48b22d35f60df39fc02e5b02dc7f40f8b44c40
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-04-17 11:38:34 -0500

    GAP-48 phrases of the steps improved

--- src/src/utilities/steps.js
+++ src/src/utilities/steps.js
@@ -1,7 +1,7 @@
 export const steps = [
   {
     title: "Select country",
-    content: "Here you can select the desire country",
+    content: "Welcome! Select the country where you want to explore crops and accessions",
     target: "#select-country",
     placement: "auto",
     spotlightClicks: true,
@@ -14,7 +14,7 @@ export const steps = [
   },
   {
     title: "Select the major crop",
-    content: "Here you can select the desire crops",
+    content: "Great! Now, choose the crops you want to explore in the selected country",
     target: "#select-majorCrop",
     placement: "auto",
     spotlightClicks: true,
@@ -25,7 +25,7 @@ export const steps = [
   },
   {
     title: "Select the landrace crop",
-    content: "Here you can select the desire landrace crops",
+    content: "Looking for a specific type of crop? Choose from our selection of traditional landraces.",
     target: "#select-landraceCrop",
     placement: "auto",
     spotlightClicks: true,
@@ -36,7 +36,7 @@ export const steps = [
   },
   {
     title: "Add to map",
-    content: "Click the button to add to map",
+    content: "Ready to map your selection? Simply click the 'Add to Map' button.",
     target: "#button-addToMap",
     placement: "auto",
     spotlightClicks: true,
@@ -47,7 +47,7 @@ export const steps = [
   },
   {
     title: "Map",
-    content: "Now you can see the layers on the map",
+    content: "Voilà! The selected crop accessions and varieties are now displayed on the map.",
     target: "#mapLayer",
     placement: "left",
     spotlightClicks: true,
@@ -57,7 +57,7 @@ export const steps = [
   },
   {
     title: "Create a route",
-    content: "Click this button to open the route section",
+    content: "Want to plan your trip? Click this button to start exploring destinations!",
     target: "#button-route",
     placement: "auto",
     spotlightClicks: true,
@@ -68,7 +68,7 @@ export const steps = [
   },
   {
     title: "Create a route",
-    content: "Write a city",
+    content: "Let's start with your first destination! Write a city to add it to your route.",
     target: "#textare-city1",
     placement: "bottom",
     spotlightClicks: true,
@@ -78,7 +78,7 @@ export const steps = [
   },
   {
     title: "Create a route",
-    content: "Add another destination",
+    content: "Ready to add more destinations? Click the plus button to keep building your route.",
     target: "#button-addDestination",
     placement: "auto",
     spotlightClicks: true,
@@ -89,7 +89,7 @@ export const steps = [
   },
   {
     title: "Create a route",
-    content: "Write another city",
+    content: "Let's keep exploring! Write another city to add to your route.",
     target: "#textare-city2",
     placement: "auto",
     spotlightClicks: true,
@@ -99,7 +99,7 @@ export const steps = [
   },
   {
     title: "Create a route",
-    content: "Now get the route!",
+    content: "You're all set! Click the 'Get Route' button to see your customized route.",
     target: "#button-getRoute",
     placement: "auto",
     spotlightClicks: true,
@@ -110,7 +110,7 @@ export const steps = [
   },
   {
     title: "Download accesion",
-    content: "Select this accesion",
+    content: "Select this accession to save it and download it later when you're ready.",
     target: ".leaflet-marker-icon",
     placement: "auto",
     spotlightClicks: true,
@@ -121,7 +121,7 @@ export const steps = [
   },
   {
     title: "Download accesion",
-    content: "Now download the accesion that you select",
+    content: "You're all set! Click the download button to get the selected accession.",
     target: "#button-downloadAccesion",
     placement: "auto",
     spotlightClicks: true,
